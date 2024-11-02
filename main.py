c28d72da949a7a582e1859aa9fbfb247f37b4936
Chris-code240 <duah.marfochristian@gmail.com>
2024-11-02 17:31:14 +0000

    Initila Commit

---
+++ main.py
@@ -0,0 +1,39 @@
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+# Load Movie dataset 'movie-recommendation' on Kaggle
+df = pd.read_csv('/kaggle/input/movie-recommendation/movie.csv')
+
+
+# Feature Extraction
+tfidf = TfidfVectorizer(stop_words='english')
+tfidf_matrix = tfidf.fit_transform(df['genres'])
+
+#Calculate Similarity between items
+cosine_sim = cosine_similarity(tfidf_matrix, tfidf_matrix)
+
+# Function to recommend movies based on title
+def recommend_movies(title, cosine_sim=cosine_sim):
+    # Get the index of the movie that matches the title
+    idx = df[df['title'] == title].index[0]
+
+    # Get the pairwise similarity scores of all movies with that movie
+    sim_scores = list(enumerate(cosine_sim[idx]))
+
+    # Sort the movies based on the similarity scores
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+
+    # Get the scores of the 3 most similar movies (excluding the movie itself)
+    sim_scores = sim_scores[1:4]
+
+    # Get the movie indices
+    movie_indices = [i[0] for i in sim_scores]
+
+    # Return the top 3 most similar movies
+    return df['title'].iloc[movie_indices]
+
+# Example Usage:
+recommended_movies = recommend_movies('Pocahontas (1995)')
+print("Movies similar to 'Pocahontas (1995)':")
+print(recommended_movies)
